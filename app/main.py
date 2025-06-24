21078593da8e6927d646ed8892d6bce8284f8626
DiWILove <povilaitusdiana@gmail.com>
2025-06-24 10:07:09 +0300

    bugs

--- app/main.py
+++ app/main.py
@@ -18,23 +18,30 @@ class CarWashStation:
         one_car_income = 0
         total_income = 0
         for car in cars:
-            if car.clean_mark <= self.clean_power:
+            if car.clean_mark < self.clean_power:
                 one_car_income = (car.comfort_class * (self.clean_power - car.clean_mark)
                                   * self.average_rating / self.distance_from_city_center)
                 car.clean_mark = self.clean_power
             total_income += one_car_income
         return round(total_income, 1)
 
+    def calculate_washing_price(self, car) -> None:
+        one_car_income = (car.comfort_class * (self.clean_power - car.clean_mark)
+                          * self.average_rating / self.distance_from_city_center)
+        return round(one_car_income, 1)
+
+    def wash_single_car(self, car):
+        return self.calculate_washing_price(car)
+
     def rate_service(self, rate: float) -> float:
         total_rate = self.average_rating * self.count_of_ratings
         self.count_of_ratings += 1
-        total_rate = (total_rate + rate) / (self.count_of_ratings)
-        return round(total_rate, 1)
+        self.average_rating = (total_rate + rate) / (self.count_of_ratings)
+        return round(self.average_rating, 1)
 
 
 bmw = Car(comfort_class=3, clean_mark=3, brand='BMW')
 audi = Car(comfort_class=4, clean_mark=2, brand='Audi')
-
 
 wash_station = CarWashStation(
     distance_from_city_center=5,
@@ -45,7 +52,6 @@ wash_station = CarWashStation(
 
 income = wash_station.serve_cars([bmw, audi])
 wash_station.rate_service(5)
-
 
 ###calculate_washing_price - method, that calculates cost for a single car
 # wash, cost is calculated as: car's comfort class * difference between
